17d97410a30686c7f74442972dbd44c1f2efb2db
harshumaretiya <harsh.patel.hp846@gmail.com>
2022-04-11 18:21:47 +0530

    New changes added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
   def index
+    @user = User.all
   end
 
   def profile
--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_081844) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
-    t.index ["owner_id"], name: "index_users_on_owner_id", unique: true
+    t.index ["owner_id"], name: "index_users_on_owner_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
