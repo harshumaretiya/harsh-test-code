1ab5f240b1bdcb14c13409624bfb52f96e22f563
harshumaretiya <harsh.patel.hp846@gmail.com>
2022-04-11 20:45:46 +0530

    heroku file host prod 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'directshift-harsh-demo.herokuapp.com', port: 5000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
